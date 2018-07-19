8b8ff68841565081c02c9e870605e8c97fb5b810
gitcaiqing <380120634@qq.com>
2018-07-19 18:25:16 +0800

    新增、修改、删除弹框处理

---
+++ src/main/resources/表结构.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `tb_basic_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增',
+  `userId` varchar(32) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL,
+  `headimg` varchar(200) DEFAULT NULL,
+  `realname` varchar(20) DEFAULT NULL,
+  `sex` int(1) DEFAULT NULL,
+  `age` int(2) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `memo` varchar(50) DEFAULT NULL,
+  `credate` datetime DEFAULT NULL,
+  `upddate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
